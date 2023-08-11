3f3048a6f3185270a3c5eda26f161dde92140a1c
Micaela <micabolivera58@gmail.com>
2023-08-11 15:51:34 -0300

    StopPropagation

--- evento.js
+++ evento.js
@@ -2,7 +2,8 @@ function mostrarAlerta() {
     alert("¡Hola!");
 }
 
-function mostrarAlertaDiv() {
+function mostrarAlertaDiv(event) {
+    event.stopPropagation(); // Detener la propagación del evento
     alert("Hola! Soy el div.");
 }
 
